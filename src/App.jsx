37acf960d06a6fba1ae2dffa105467251ace655e
Raghav Arora <raghavarora9414@gmail.com>
2026-04-03 12:14:15 +0530

    Add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./Components/NavBar"
+import SideBar from "./Components/SideBar"
 function App() {
   
   return (
-    <div className="min-h-screen w-full bg-surface text-[#F1E9E9] font-sans">
+    <div className="h-screen w-full bg-surface text-on-surface font-sans flex flex-col">
         <NavBar/>
+        <div className="flex flex-row flex-1 overflow-hidden">
+          <SideBar/>
+          <main className="flex-1 overflow-y-auto p-4">
+            <Outlet/>
+          </main>
+        </div>
     </div>
   )
 }
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,26 @@
+import { HomeIcon } from "@heroicons/react/24/outline"
+import { Cog6ToothIcon } from "@heroicons/react/24/outline"
+import { UserIcon } from "@heroicons/react/24/outline"
+function SideBar(){
+    return(
+        <div className="hidden md:flex w-18 border-r-4 border-primary p-2 flex-col justify-between">
+
+            <div className="h-44 w-full flex flex-col justify-between items-center">
+                <div className="text-on-surface h-15 w-full flex flex-col items-center justify-center hover:scale-105 ease-in-out duration-300 hover:text-accent">
+                    <HomeIcon className="w-7 h-7 "/>
+                    <p>Home</p>
+                </div>
+                <div className="text-on-surface h-15 w-full flex flex-col items-center justify-center hover:scale-105 ease-in-out duration-300 hover:text-accent">
+                    <UserIcon className="w-7 h-7"/>
+                    <p>Profile</p>
+                </div>
+            </div>
+            <div className="text-on-surface h-20 w-full flex flex-col items-center justify-center">
+                <div className="w-[90%] border-t-2 border-gray-500 mb-4"></div>
+                <Cog6ToothIcon className="w-7 h-7"/>
+                <p>Settings</p>
+            </div>
+        </div>
+    )
+}
+export default SideBar
