a16fe0aa06d1b0ae0dd19e3c4294839f2f2be818
Raghav Arora <raghavarora9414@gmail.com>
2026-04-11 00:15:25 +0530

    icon fix

--- src/Components/Modals/ProjectModal.jsx
+++ src/Components/Modals/ProjectModal.jsx
@@ -31,7 +31,8 @@ function ProjectModal({isOpen,onClose}){
             <div className="fixed inset-0 bg-black/50 backdrop-blur-sm z-40" onClick={onClose}></div>
 
             <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-surface rounded-2xl shadow-xl p-6 flex flex-col gap-4 lg:w-xl md:w-lg">
-                <div className="w-full flex justify-center">
+                <div className="w-full flex justify-between items-center">
+                    <p>Add details...</p>
                     <XMarkIcon className="w-7 h-7 cursor-pointer hover:text-red-400" onClick={onClose} />
                 </div>
 
